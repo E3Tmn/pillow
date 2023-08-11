8ccdc468ffda54a67381fb136f25f419f4e1a5be
E3Tmn <nick1tapinyagin@yandex.ru>
2023-08-11 13:55:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ rgb_image = image.convert("RGB")
 
 red, green, blue = image.split()
 
-coordinates_red = (40, 0, image.width, image.height)
-coordinates_green = (20, 0, image.width - 20, image.height)
-coordinates_blue = (0, 0, image.width - 40, image.height)
+red_left = red.crop((40, 0, image.width, image.height)) 
+red_middle = red.crop((20, 0, image.width - 20, image.height))
+blue_right = blue.crop((0, 0, image.width - 40, image.height))
+blue_middle = blue.crop((20, 0, image.width - 20, image.height))
+green = green.crop((20, 0, image.width - 20, image.height))
 
-red = red.crop(coordinates_red) 
-green = green.crop(coordinates_green)
-blue = blue.crop(coordinates_blue)
-
+red = Image.blend(red_left, red_middle, 0.5)
+blue = Image.blend(blue_right, blue_middle, 0.5)
 new_image = Image.merge("RGB", (red, green,blue))
 new_image.thumbnail((80,80))
 new_image.save('newMonro.jpg')
